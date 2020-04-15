d9dd8be36bbda1022e2826c76d974969385337c4
8hfq <w@8hfq.com>
2020-04-15 23:00:13 +0800

    add

---
+++ 114.flatten-binary-tree-to-linked-list.py
@@ -0,0 +1,75 @@
+#
+# @lc app=leetcode id=114 lang=python3
+#
+# [114] Flatten Binary Tree to Linked List
+#
+# https://leetcode.com/problems/flatten-binary-tree-to-linked-list/description/
+#
+# algorithms
+# Medium (47.02%)
+# Likes:    2328
+# Dislikes: 283
+# Total Accepted:    314.7K
+# Total Submissions: 667.4K
+# Testcase Example:  '[1,2,5,3,4,null,6]'
+#
+# Given a binary tree, flatten it to a linked list in-place.
+# 
+# For example, given the following tree:
+# 
+# 
+# ⁠   1
+# ⁠  / \
+# ⁠ 2   5
+# ⁠/ \   \
+# 3   4   6
+# 
+# 
+# The flattened tree should look like:
+# 
+# 
+# 1
+# ⁠\
+# ⁠ 2
+# ⁠  \
+# ⁠   3
+# ⁠    \
+# ⁠     4
+# ⁠      \
+# ⁠       5
+# ⁠        \
+# ⁠         6
+# 
+# 
+#
+
+# @lc code=start
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
+
+class Solution:
+    def flatten(self, root: TreeNode) -> None:
+        """
+        Do not return anything, modify root in-place instead.
+        """
+        if not root:
+            return 
+        if root.left:
+            self.flatten(root.left)
+        if root.right:
+            self.flatten(root.right)
+        tmp = root.right
+        root.right = root.left
+        cur = root
+        while cur.right:
+            cur = cur.right
+        cur.right = tmp
+        root.left = None
+
+
+# @lc code=end
+
