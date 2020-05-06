41b6d10389fd09801908dcecc9f06330582ad0ae
8hfq <w@8hfq.com>
2020-05-06 17:50:03 +0800

    add

--- 25.reverse-nodes-in-k-group.py
+++ 25.reverse-nodes-in-k-group.py
@@ -13,6 +13,28 @@
 
 class Solution:
     def reverseKGroup(self, head: ListNode, k: int) -> ListNode:
-        
+        dummy = ListNode(0)
+        p = dummy
+        while True:
+            count = k
+            stack = []
+            tmp = head
+            while count and tmp:
+                stack.append(tmp)
+                tmp = tmp.next
+                count -=1
+
+            if count:
+                p.next = head
+                break
+            while stack:
+                p.next = stack.pop()
+                p = p.next 
+            
+            p.next = tmp
+            head = tmp
+        return dummy.next
+
+ 
 # @lc code=end
 
