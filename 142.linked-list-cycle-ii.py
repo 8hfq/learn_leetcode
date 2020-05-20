4eeac3ca5726720ae58297830169a42d7934ed5a
8hfq <w@8hfq.com>
2020-05-20 20:01:29 +0800

    add

--- 142.linked-list-cycle-ii.py
+++ 142.linked-list-cycle-ii.py
@@ -72,6 +72,15 @@
 
 class Solution:
     def detectCycle(self, head: ListNode) -> ListNode:
-        
+        slow,fast = head,head
+        while fast and fast.next:
+            slow = slow.next
+            fast =fast.next.next
+            if slow ==fast:
+                slow2 = head
+                while slow!=slow2:
+                    slow = slow.next
+                    slow2= slow2.next
+                return slow
 # @lc code=end
 
