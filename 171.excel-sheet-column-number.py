f3a7da668913c2cb4ce385b15336cb69bae15b36
8hfq <w@8hfq.com>
2020-05-29 15:27:07 +0800

    add

---
+++ 171.excel-sheet-column-number.py
@@ -0,0 +1,62 @@
+#
+# @lc app=leetcode id=171 lang=python3
+#
+# [171] Excel Sheet Column Number
+#
+# https://leetcode.com/problems/excel-sheet-column-number/description/
+#
+# algorithms
+# Easy (53.90%)
+# Likes:    883
+# Dislikes: 147
+# Total Accepted:    278.7K
+# Total Submissions: 517K
+# Testcase Example:  '"A"'
+#
+# Given a column title as appear in an Excel sheet, return its corresponding
+# column number.
+# 
+# For example:
+# 
+# 
+# ⁠   A -> 1
+# ⁠   B -> 2
+# ⁠   C -> 3
+# ⁠   ...
+# ⁠   Z -> 26
+# ⁠   AA -> 27
+# ⁠   AB -> 28 
+# ⁠   ...
+# 
+# 
+# Example 1:
+# 
+# 
+# Input: "A"
+# Output: 1
+# 
+# 
+# Example 2:
+# 
+# 
+# Input: "AB"
+# Output: 28
+# 
+# 
+# Example 3:
+# 
+# 
+# Input: "ZY"
+# Output: 701
+# 
+#
+
+# @lc code=start
+class Solution:
+    def titleToNumber(self, s: str) -> int:
+        res = 0
+        for i in s:
+            res = res*26 +ord(i)-ord('A')+1
+        return res
+# @lc code=end
+
